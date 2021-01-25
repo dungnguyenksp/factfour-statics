46b812c897ef266a8d37ba3a916377ae56a5638f
dung.nguyen <dung.nguyen@groovetechnology.com>
2021-01-25 17:28:31 +0700

    Learn useEffect() - get a postlist from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import PostList from "./components/PostList";
 
 function App() {
+  const listposts = [
+    {
+      id: 1,
+      title: "Post 1",
+      status: true,
+    },
+    {
+      id: 2,
+      title: "Post 2",
+      status: true,
+    },
+    {
+      id: 3,
+      title: "Post 3",
+      status: true,
+    },
+  ];
+
+  const [postlist, setPostlist] = useState(listposts);
+
+  useEffect(() => {
+    const getListPosts = async () => {
+      try {
+        const requestURL =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const request = await fetch(requestURL);
+        const results = await request.json(request);
+        console.log(results);
+        setPostlist(results.data);
+      } catch (error) {
+        console.log("Fail get data" + error.message);
+      }
+    };
+    getListPosts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PostList posts={postlist} />
     </div>
   );
 }
